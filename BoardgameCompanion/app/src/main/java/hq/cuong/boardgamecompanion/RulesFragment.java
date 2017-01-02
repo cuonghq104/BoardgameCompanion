033d203bc22cd0633f6666786e2325c388293993
Ziggy192 <luuquangnghia97@gmail.com>
2017-01-02 13:32:50 +0700

    done testing markdown and searchBar

--- BoardgameCompanion/app/src/main/java/hq/cuong/boardgamecompanion/RulesFragment.java
+++ BoardgameCompanion/app/src/main/java/hq/cuong/boardgamecompanion/RulesFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +14,10 @@ import android.webkit.WebViewClient;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import hq.cuong.boardgamecompanion.adapters.SingleBoardGameAdapter;
 import hq.cuong.boardgamecompanion.models.BoardGame;
+import us.feras.mdv.MarkdownView;
 
 
 /**
@@ -27,8 +31,12 @@ public class RulesFragment extends Fragment {
 //    @BindView(R.id.wv_detail)
 //    WebView wvDetail;
 
-    @BindView(R.id.rv_rules)
-    RecyclerView rvRules;
+    //    @BindView(R.id.rv_rules)
+//    RecyclerView rvRules;
+    @BindView(R.id.mdv_rules)
+    MarkdownView mdvRules;
+    @BindView(R.id.sv_rules)
+    SearchView svRules;
 
     public RulesFragment() {
         // Required empty public constructor
@@ -58,12 +66,49 @@ public class RulesFragment extends Fragment {
 
     }
 
+    @OnClick(R.id.btn_find_next)
+    public void findNext() {
+        mdvRules.findNext(true);
+    }
     private void setupUI() {
-        adapter = new SingleBoardGameAdapter();
+        svRules.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d("ahihi", String.format("begin search : query  = %s", query));
+                mdvRules.findAllAsync(query);
+                mdvRules.findNext(true);
+                return false;
+            }
 
-        rvRules.setLayoutManager(new LinearLayoutManager(getContext()));
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        svRules.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        rvRules.setAdapter(adapter);
+
+            }
+        });
+//        mdvRules.loadMarkdown("Here's our logo (hover to see the title text):\n" +
+//                "\n" +
+//                "Inline-style: \n" +
+//                "![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png \"Logo Title Text 1\")\n" +
+//                "\n" +
+//                "Reference-style: \n" +
+//                "![alt text][logo]\n" +
+//                "\n" +
+//                "[logo]: https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png \"Logo Title Text 2\"");
+//        mdvRules.loadMarkdownFile("http://m.uploadedit.com/ba3s/1483326555496.txt");
+        mdvRules.loadMarkdownFile( "file:///android_asset/werewolf_one_night.txt",
+                "file:///android_asset/my_css.css");
+        //todo debugging
+
+//        adapter = new SingleBoardGameAdapter();
+//        rvRules.setLayoutManager(new LinearLayoutManager(getContext()));
+//        rvRules.setAdapter(adapter);
 
 //        WebViewClient webViewClient = new WebViewClient();
 //        wvDetail.setWebViewClient(webViewClient);
